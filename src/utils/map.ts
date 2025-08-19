4be777330742bfbee6cfec990ffc1778ae1dab8f
Sebastian Ellefsen <sebastian@luxsave.com>
2025-08-19 19:32:59 +0200

    Add drag-and-drop functionality

--- src/utils/map.ts
+++ src/utils/map.ts
@@ -2,6 +2,10 @@ import { Map, View } from 'ol'
 import TileLayer from 'ol/layer/Tile'
 import { useGeographic } from 'ol/proj'
 import { OSM } from 'ol/source'
+import { DragAndDrop } from 'ol/interaction'
+import { GPX, GeoJSON, KML } from 'ol/format'
+import VectorSource from 'ol/source/Vector'
+import VectorLayer from 'ol/layer/Vector'
 
 export function initMap(root: string) {
   useGeographic()
@@ -12,4 +16,23 @@ export function initMap(root: string) {
     layers: [new TileLayer({ source: new OSM() })],
   })
 
+  let featureLayers: VectorLayer[] = []
+
+  const interaction = new DragAndDrop({ formatConstructors: [GPX, GeoJSON, KML] })
+  interaction.on('addfeatures', (ev) => {
+    // Remove old features
+    featureLayers.forEach((layer) => map.removeLayer(layer))
+    featureLayers = []
+
+    const vectorSource = new VectorSource({
+      features: ev.features,
+    })
+
+    featureLayers.push(new VectorLayer({ source: vectorSource }))
+    featureLayers.forEach((layer) => map.addLayer(layer))
+
+    map.getView().fit(vectorSource.getExtent())
+  })
+
+  map.addInteraction(interaction)
 }
